c4e3cc420cd44445e27b07a89d6859d71293d672
Angelo <angelonicolosi@yanchware.com>
2015-04-23 22:47:36 +0200

    Added categories list back to blog page

--- sidebar-blog.php
+++ sidebar-blog.php
@@ -1,7 +1,19 @@
    <!-- Sidebar -->
-    <aside class="large-3 columns"> 
-      <div class="panel text-center">
-      <h5><?php _e('Random Post', 'noborders');?></h5>
+    <aside class="large-3 columns">
+	    <h5><?php _e('Blog Topics', 'noborders');?></h5>
+	    <ul class="side-nav">
+		  <?php
+		  $args = array(
+		    'orderby' => 'name',
+		    'order' => 'ASC',
+		    'parent' => 9
+		    );
+		  $categories = get_categories($args);
+		  foreach($categories as $category) { 
+		      echo '<li><a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->name.'</a></li>';  } 		  ?>
+	    </ul> 
+		<div class="panel text-center">
+		<h5><?php _e('Random Post', 'noborders');?></h5>
 		<p>
 		<?php
 			query_posts(array(
@@ -21,8 +33,8 @@
 			<?php the_excerpt(); ?>
 			</div>
 		<?php endwhile; ?>
-	    </p>
-		
-      </div>
+		</p>
+
+		</div>
     </aside>
     <!-- End Sidebar -->
